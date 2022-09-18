b20b125b09b7a8fccdce448faa102f8bcce37ba3
Elena Carey <theelenacarey@gmail.com>
2022-09-18 12:43:22 -0500

    Still trying.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 					<div className="container">
 						<Routes>
               <Route path="/" element={<About />} />
-							<Route path="about" index element={<About />} />
+							<Route path="about" element={<About />} />
 							<Route path="portfolio" element={<Portfolio />} />
 							<Route path="contact" element={<Contact />} />
 							<Route path="resume" element={<Resume />} />
